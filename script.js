1ae58f10bb227bd133587f1500c5d670e9322acc
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-02-29 15:29:13 +0100

    ok

--- script.js
+++ script.js
@@ -86,3 +86,25 @@ $(document).ready(function() {
         }, 1000);
     });
 });
+
+
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+
+scrollToTopBtn.onclick = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
